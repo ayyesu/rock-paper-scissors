ecc79899c51c175213a302aedb8cf53e4516f61b
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-10-18 02:41:58 +0000

    modified:   play_func.js

---
+++ play_func.js
@@ -0,0 +1,80 @@
+let comChoice = {Value: "this"};
+let playerChoice;
+let compChoiceNum = 0;
+let playerChoiceNum = 0;
+const buttons = document.querySelectorAll('.btn');
+
+let playerScore = 0;
+let compScore = 0;
+
+const player = document.querySelector('#player-score');
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector("#comp-score");
+computer.textContent = `Computer Score: ${compScore}`;
+
+const output = document.querySelector("#output");
+output.textContent = "May the Best Person Win!"
+
+buttons.forEach((button) => {button.addEventListener('click', () => {
+
+    playerChoice = button.id;
+    if (playerChoice == "rock") {
+        playerChoiceNum = 0;
+    } else if (playerChoice == "paper") {
+        playerChoiceNum = 1;
+    } else if (playerChoice == "scissors") {
+        playerChoiceNum = 2;
+    }
+    computerChoiceNum = computerPlay(compChoice);
+    playGame();
+    })
+})
+
+function computerPlay(compChoice){
+    let choiceNum = Math.floor(Math.random() * 3);
+    if (choiceNum == 0) {
+        compChoice.Value = "rock";
+    } else if (choiceNum == 1) {
+        compChoice.Value = "paper"
+    } else if (choiceNum  == 2) {
+        compChoice.Value = "scissors";
+    }
+    return choiceNum;
+}
+
+function playRound(){
+    let win_array = [[0, 2, 1],
+                    [1, 0, 2],
+                    [2, 1, 0]];
+    let result = win_array[playerChoiceNum][compChoiceNum];
+        if (result == 0){
+            output.textContent = `Its a tie! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
+        } else if (result == 1){
+            output.textContent = `You won! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
+            playerScore++;
+        } else if (result == 2){
+            output.textContent = `You lost! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
+            compScore++;
+        }
+}
+
+function playGame() {
+    output.textContent = "Choose Rock, Paper, or Scissors";
+    playRound();
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${compScore}`;
+    if (playerScore == 5) {
+        output.textContent = "Yayyyy You Won!!";
+        playerScore = 0;
+        compScore = 0;
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
+    } else if (compScore == 5){
+        output.textContent = "You Lost the game:/ You are not wiser than the computer";
+        playerScore = 0;
+        compScore = 0;
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
+    }
+}
